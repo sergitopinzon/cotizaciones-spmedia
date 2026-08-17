0d427d99057c7b51cd61415e377b11e6cef89457
Sergio Pinzon <298451535+sergitopinzon@users.noreply.github.com>
2026-08-17 04:44:00 -0500

    Estados+seguimiento, duplicar, buscar, descuento %, resumen mensual, respaldo Excel

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'cot-spmedia-v16';
+const CACHE = 'cot-spmedia-v17';
 const ASSETS = ['./', './index.html', './sp_media_logo.png', './icon-192.png', './icon-512.png', './manifest.json', './firma.png?v=2'];
 
 self.addEventListener('install', e => {
